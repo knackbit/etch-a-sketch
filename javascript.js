cfe57c2a5cb2cf2a8657b99879489a266a31e599
Cory Faber <cory.j.faber@gmail.com>
2023-08-30 16:12:13 -0500

    add pseudocode for the backend logic of the etch a sketch

---
+++ javascript.js
@@ -0,0 +1,38 @@
+/*
+Etch a sketch program
+
+set variable gridSize = 16
+
+function createGrid(gridSize)
+    set variables currentHeight and currentWidth equal to 0
+    loop equal to number of gridSize times add to current height
+        loop through the gridsize equal to width minus one times, add to current width
+            set variable to createDiv(currentHeight, CurrentWidth)
+            make the div not have a new line
+        set variable to createDiv(currentHeight, CurrentWidth)
+        Div should create new line on next element
+        
+
+function createDiv(currentHeight, currentWidth):
+    divElement  = create div element
+    and set id of the div to "height-" + currentHeight + "-width-" + currentWidth
+    append divElement to grid
+    add event when mouse is hovered over div to change the background color from white to black
+    return divElement
+
+add button to top of screen that will send the user a popup asking for the number of squares per side of the new grid
+
+
+button = select create-grid button from dom
+add a click event to button that prompts user for to input one number for the new grid size
+    IF input number is an integer between 1 and 100
+        return an error and prompt user again for input
+    ELSE
+        run function to remove current grid
+        run function to create new grid and pass it the input number
+
+function removeGrid
+    remove all divs that are a child of #grid-container
+
+
+*/
